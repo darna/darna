0e2476080533f87ff1a9d604e29ace762d5199f2
Alvin Garcia <alving@sourcepad.com>
2014-10-03 21:23:24 +0800

    ping pong

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'static#index'
 
+  get 'api/p/:project_id/ping', to: 'things#ping'
   get 'api/p/:project_id/:id', to: 'things#get_api'
   post 'api/p/:project_id', to: 'things#post_api'
 end
